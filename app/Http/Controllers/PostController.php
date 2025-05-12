523f7049b498cc49550d142417e412b72c4c3526
svitorz <vitor.fabio.castro@gmail.com>
2025-05-12 20:08:38 -0300

    feat: função para criar e listar

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $post = new Post;
+        $posts = $post->all();
+
+        return view('components.posts.show-all-posts', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('components.posts.create-post');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'titulo' => 'required|string|min:3|max:128',
+            'conteudo' => 'required|string|min:10|max:1024',
+        ]);
+
+        $post = new Post;
+        $post->user_id = auth()->id();
+        $post->titulo = $validated['titulo'];
+        $post->conteudo = $validated['conteudo'];
+        $post->save();
+
+        return $this->index();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
