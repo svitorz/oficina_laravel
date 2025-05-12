d564ff42e91c3226ac9d41f0258f398f8e7218b3
svitorz <vitor.fabio.castro@gmail.com>
2025-05-12 20:46:10 -0300

    feat: editar postagem

--- resources/views/components/posts/show-all-posts.blade.php
+++ resources/views/components/posts/show-all-posts.blade.php
@@ -7,6 +7,12 @@
             <h1>{{ $post->titulo }}</h1>
             <p>{{ $post->conteudo }}</p>
             <p>{{ $post->user->name }}</p>
+            <a href="{{ route('posts.edit', $post->id) }} ">Editar</a>
+            <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                @method('delete')
+                @csrf
+                <button type="submit">Deletar</button>
+            </form>
         </div>
     @endforeach
 </div>
